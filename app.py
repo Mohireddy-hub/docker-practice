726eb5360365cf3137efa84a5f7dcd3144d002dd
Mohireddy-hub <mohithareddykolli98@gmail.com>
2025-08-16 14:22:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def home():
     return "Hello from Flask!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Change here: host 0.0.0.0 and port 8080
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
